2bb7408b210d142a4212f9acc2ad7bbdcb1033d9
Aviel Warschawski <mail@aviel.org>
2018-08-27 10:46:04 +0300

    Supporting Python 3.7

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@
 
 from distutils.core import setup
 
-VERSION = "3.3.2"
+VERSION = "3.3.3"
 
 setup(
     name = 'kplugs',
-    python_requires='==3.6.*',
+    python_requires='==3.7.*',
     packages = ['kplugs'],
     version = VERSION,
     description = 'The KPlugs Python module',
     author = 'Aviel Warschawski',
     author_email = 'contact@kplugs.org',
-    url = 'https://github.com/avielw/kplugs-python3.6',
+    url = 'https://github.com/avielw/kplugs-python3.7',
     keywords = [
         'kplugs',
         'kernel',
@@ -22,6 +22,6 @@ setup(
     classifiers = [
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
